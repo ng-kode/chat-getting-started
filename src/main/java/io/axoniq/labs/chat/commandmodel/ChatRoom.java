701ba613ef27fccbb98fc13d2802daf009a0b8c6
Richard Ng <richard.ng.cccc@gmail.com>
2020-05-21 23:29:00 +0800

    first commit

---
+++ src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
@@ -0,0 +1,15 @@
+package io.axoniq.labs.chat.commandmodel;
+
+import io.axoniq.labs.chat.coreapi.*;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.axonframework.modelling.command.AggregateLifecycle.apply;
+
+public class ChatRoom {
+    // TODO
+}
