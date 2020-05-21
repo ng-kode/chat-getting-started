2790f37e69c9ba3ada3877dbf73a88a6dedbdee8
Richard Ng <richard.ng.cccc@gmail.com>
2020-05-21 23:29:22 +0800

    Implement aggregate ChatRoom

--- src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
+++ src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
@@ -11,5 +11,51 @@ import java.util.List;
 import static org.axonframework.modelling.command.AggregateLifecycle.apply;
 
 public class ChatRoom {
-    // TODO
+    @AggregateIdentifier
+    private String roomId;
+    private List<String> participants = new ArrayList<>();
+
+    @CommandHandler
+    public ChatRoom(CreateRoomCommand cmd) {
+        apply(new RoomCreatedEvent(cmd.getRoomId(), cmd.getName()));
+    }
+
+    protected ChatRoom() {}
+
+    @CommandHandler
+    public void handle(JoinRoomCommand cmd) {
+        if (!participants.contains(cmd.getParticipant())) {
+            apply(new ParticipantJoinedRoomEvent(cmd.getRoomId(), cmd.getParticipant()));
+        }
+    }
+
+    @CommandHandler
+    public void handle(LeaveRoomCommand cmd) {
+        if (participants.contains(cmd.getParticipant())) {
+            apply(new ParticipantLeftRoomEvent(cmd.getRoomId(), cmd.getParticipant()));
+        }
+    }
+
+    @CommandHandler
+    public void handle(PostMessageCommand cmd) {
+        if (!participants.contains(cmd.getParticipant())) {
+            throw new IllegalStateException("Participant can post message only if he/she is in the room");
+        }
+        apply(new MessagePostedEvent(cmd.getRoomId(), cmd.getParticipant(), cmd.getMessage()));
+    }
+
+    @EventSourcingHandler
+    public void handle(RoomCreatedEvent event) {
+        roomId = event.getRoomId();
+    }
+
+    @EventSourcingHandler
+    public void handle(ParticipantJoinedRoomEvent event) {
+        participants.add(event.getParticipant());
+    }
+
+    @EventSourcingHandler
+    public void handle(ParticipantLeftRoomEvent event) {
+        participants.remove(event.getParticipant());
+    }
 }
